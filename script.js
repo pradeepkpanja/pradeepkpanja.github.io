bf79eb18b955850ab354fcba1876fe5127277566
pradeep3443 <panja1699@gmail.com>
2024-03-13 19:13:51 -0400

    "Updated Styling and Icons"

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+let text = ['A Computer Engineer' ,'A Full Stack Developer','An aspiring Learner'];
+let textTodisplay = document.getElementById('textToDisplay');
+let currentLetter = 0;
+let currentText = 0;
+
+function displayText() {
+
+    if (currentLetter === 0) {
+        textToDisplay.textContent = '';
+    }
+
+    if (currentText >= text.length) {
+        currentText = 0; 
+    }
+
+    if (currentLetter < text[currentText].length) {
+        textToDisplay.textContent += text[currentText][currentLetter];
+        currentLetter++;
+    } else {
+        currentLetter = 0;
+        currentText++;
+    }
+  
+    
+}
+setInterval(displayText, 250);
